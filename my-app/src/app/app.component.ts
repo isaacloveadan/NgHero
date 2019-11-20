d7a02093a2db01e221cd30e2c548dbae212488ae
isaac <273070111@qq.com>
2019-11-20 17:36:44 +0800

    增加注释

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // 是一个css选择器 一旦在模板 HTML 中找到了这个选择器对应的标签，就创建并插入该组件的一个实例
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   // 组件要先在这里声明
+  // 声明属于本NgModule的组件、指令、管道
   declarations: [
     AppComponent,
     HeroesComponent,
@@ -26,6 +27,7 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     DashboardComponent,
     HeroSearchComponent,
   ],
+  // exports（导出表）：能在其他模块的组件模板中使用的可声明对象的子集
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -37,7 +39,9 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
       InMemoryDataService, { dataEncapsulation: false }
     )
   ],
+  // 向全局服务中贡献的那些服务的创建器
   providers: [],
+  // 应用的主视图 称为根组件 只有根模块才会设置这个bootstrap树形
   bootstrap: [AppComponent]
 })
 export class AppModule { }
